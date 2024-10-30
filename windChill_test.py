0b533a37620663cdc29c750101feb270b5411ab1
bigbandzjosh <joosh.scott@hotmail.com>
2024-10-29 23:18:19 -0300

    done for tonight:)

--- windChill_test.py
+++ windChill_test.py
@@ -4,4 +4,4 @@ import windChill
 class TestWindChill(unittest.TestCase):
     def test_windChill(self):
        self.assertEqual(windChill.WindChill(-10, 1), -10.587)
-       self.assertEqual(windChill.WindChill(-10, 5), -15.097)
+       self.assertAlmostEqual(windChill.WindChill(-10, 6), -13.521, places=3)
