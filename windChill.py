5aab6962a0cecd5f9e159145a9faa07a69cecdf0
bigbandzjosh <joosh.scott@hotmail.com>
2024-11-03 21:50:32 -0400

    assessment completed, the 0 < wind_speed < 5 was the confusing part

--- windChill.py
+++ windChill.py
@@ -25,34 +25,37 @@ class WindSpeed:
 
     def convert_to_knots(self):
         if self.unit == 'kmh':
+            print(self.wind_speed, "kmh")
             return self.wind_speed / 1.852
         return self.wind_speed
 
 def WindChill(temperature, wind_speed, temp_unit='C', speed_unit='kmh'):
     temperature = Temperature(temperature, temp_unit)
-    wind_speed = WindSpeed(wind_speed, speed_unit)
+    wind = WindSpeed(wind_speed, speed_unit)
 
     temp = temperature.convert_to_celsius()
-    wind_speed = wind_speed.convert_to_kmh()
-
-    if temp <= 0 and wind_speed >= 5:
-        windchill = 13.12 + 0.6215 * temp - 11.37 * wind_speed**0.16 + 0.3965 * temp * wind_speed**0.16
+    wind_speed = wind.convert_to_kmh()
+    
+    if wind_speed == 0:
+        return None
+    elif temp <= 0 and wind_speed >= 5:
+        windchill = (13.12 + 0.6215 * temp - 11.37 * wind_speed**0.16 + 0.3965 * temp * wind_speed**0.16)
+        return windchill
+    elif temp <= 0 and 0 < wind_speed < 5:
+        windchill = (temp + ((-1.59 + 0.1345 * temp) / 5) * wind_speed)
         return windchill
     else:
-        windchill = temp + ((-1.59 + 0.1345 * temp) / 5) * wind_speed
-        return windchill
+        return None
     
-   
-
 # Example usage
-temperature = -10
-wind_speed = 1
+temperature = 10
+wind_speed = 0.539957
 
-print(WindChill(temperature, wind_speed, 'C', 'kmh'))
+print(WindChill(temperature, wind_speed, 'C', 'knots'))
 print(WindChill(-10, 0.539957, 'C', 'knots'))
 print(WindChill(14, 1, 'F', 'kmh'))
 print(WindChill(-10, 6, 'C', 'kmh'))
-print(WindChill(10, 0.539957, 'C', 'knots'))
+print(WindChill(-10, 1, 'C', 'kmh'))
 
 
 
--- windChill_test.py
+++ windChill_test.py
@@ -4,4 +4,17 @@ import windChill
 class TestWindChill(unittest.TestCase):
     def test_windChill(self):
        self.assertEqual(windChill.WindChill(-10, 1), -10.587)
-       self.assertAlmostEqual(windChill.WindChill(-10, 6), -13.521, places=3)
+
+    def test_windChill2(self):
+        self.assertAlmostEqual(windChill.WindChill(-10, 0.539957, 'C', 'knots'), -10.587, places=3)
+
+    def test_windChill3(self):
+        self.assertEqual(windChill.WindChill(14, 1, 'F', 'kmh'), -10.587)
+
+    def test_windChill4(self):
+        self.assertAlmostEqual(windChill.WindChill(-10, 6, 'C', 'kmh'), -13.521, places=3)
+
+    def test_windChill5(self):
+        self.assertIsNone(windChill.WindChill(10, 0.539957, 'C', 'knots'), None)
+        
+
